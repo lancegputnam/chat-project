de58b7050518856dce811e00e9f8b4f65e038485
Lance Putnam <lancegputnam@gmail.com>
2014-05-31 23:19:10 -0400

    Test Additions

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,12 +3,63 @@
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-              var answer = 2;
-              expect(2).to.equal(answer);
-            });
-        });
+    describe("login event", function () {
+        it('should not accept an empty string or max character as a user name', function () {
+        	var emptyString = function() {
+					var objUser = new User('');
+				};
+
+				expect(emptyString).to.throw(Error);
+      });
+
+        it('should not accept special characters for a user name.', function () {
+        	var specialCharacters = function() {
+					var objUser = new User('$&%#@!');
+				};
+
+				expect(specialCharacters).to.throw(Error);
+      });
+
+        it('should not accept a user name without letters', function () {
+        	var emptyLetters = function() {
+					var objUser = new User('12345');
+				};
+
+				expect(emptyLetters).to.throw(Error);
+    	});
+
+        it('should require the string to be at least two characters in length', function() {
+				var userNameLength = function() {
+					var objUser = new User('1');
+				};
+
+				expect(userNameLength).to.throw(Error);
+		});
+    }); 
+
+    describe("posted message", function () {
+    	it('should display the username, message and time in the message container', function () {
+    		var userName = "lancegputnam";
+    		var message = "message will be displayed here";
+    		var time = "5:00pm";
+    		var messageInstance = new message ("userName, message, time")
+
+    		expect(messageInstance).to.equal("userName, message, time");
+    	});
+
+    	it('should display the username', function () {
+    		var messageInstance = new message (user, message, time)
+    		expect(messageInstance.user).to.equal("lancegputnam");
+    	});
+    	
+    	it('should display the message', function () {
+    		var messageInstance = new message (user, message, time)
+    		expect(messageInstance.message).to.equal("message will be displayed here");
+    	});
+
+    	it('should display the time', function () {
+    		var messageInstance = new message (user, message, time)
+    		expect(messageInstance.time).to.equal("5:00pm");
+    	});
     });
 })();
